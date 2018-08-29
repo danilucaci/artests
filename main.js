69892c8ca990017e7a7698dd2a9815ff79287ef8
Dani Lucaci <danilucaci@gmail.com>
2018-08-29 11:54:54 +0200

    Clear unnecesary stuff

--- main.js
+++ main.js
@@ -1,2 +1,50 @@
 "use strict";
-console.log("Hello Ba");
+
+var euComStorage = function () {
+  var UIClasses = {
+    menuBtn: ".a-menu__btn"
+  };
+
+  var UISelects = {
+    menuBtn: document.querySelector(UIClasses.menuBtn)
+  };
+
+  return {
+    getSelects: function getSelects() {
+      return UISelects;
+    },
+    getClasses: function getClasses() {
+      return UIClasses;
+    }
+  };
+}();
+
+/****************************************************************
+ ************************ Global Page Control ***********************/
+
+var euCom = function (euComStorage) {
+  var UISelects = euComStorage.getSelects();
+  var UIClasses = euComStorage.getClasses();
+
+  // ********************************************************
+  // ********** Functions
+
+  function atachEventlisteners() {
+    console.log("HEHE;");
+  }
+
+  return {
+    initUI: function initUI() {
+      console.groupCollapsed("%c Incepem:", "color: #3DAEFF");
+      console.log("%c Gata.", "color: #79E36B");
+      console.groupEnd();
+
+      atachEventlisteners();
+    }
+  };
+}(euComStorage);
+
+/****************************************************************
+ ************************ Global Page Init **********************/
+
+euCom.initUI();
