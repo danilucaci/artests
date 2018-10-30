8d5250d53b9b4ba1cf8b2561193e3b2bd74f3015
Dani Lucaci <danilucaci@gmail.com>
2018-10-30 14:47:10 +0100

    push to github

--- main.js
+++ main.js
@@ -1,43 +1,8 @@
 "use strict";
 
-var Storage = function () {
-  var UISelects = {
-    card: document.querySelector(".card"),
-    inner: document.querySelector(".inner")
-  };
-
-  return {
-    getSelects: function getSelects() {
-      return UISelects;
-    }
-  };
-}();
-
-/********************************************************************
- ************************ Global Page Control ***********************/
-
-var Page = function (Storage) {
-  var UISelects = Storage.getSelects();
-
-  // ********************************************************
-  // Functions
-
-  function addEventListeners() {
-    UISelects.card.addEventListener("click", grow);
+AFRAME.registerComponent("foo", {
+  init: function init() {
+    var proba = document.querySelector("#toMonkey").src;
+    console.log(proba);
   }
-
-  function grow() {
-    UISelects.inner.classList.toggle("grow");
-  }
-
-  return {
-    initUI: function initUI() {
-      addEventListeners();
-    }
-  };
-}(Storage);
-
-// ****************************************************************
-// Global Page Init
-
-Page.initUI();
+});
